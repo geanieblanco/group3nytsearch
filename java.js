554a398b2ca6ed1a7324a05b792c186954d58e18
geanie blanco <geanieblanco@gmail.com>
2017-09-20 20:43:57 -0400

    java file update

--- java.js
+++ java.js
@@ -1,6 +1,8 @@
 var apiKey: "bb138c818e634e74a55413b6d15ac2fd";
 
-var queryURL: "http://api.nytimes.com/svc/archive/v1/ "+ year +"/ "+ month +".json?api-key="+ apiKey +"";
+var queryURL: ;
+
+var keyword:
 
 var year: 
 
